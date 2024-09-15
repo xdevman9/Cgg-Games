be2f85a45efbc1cd253c6ea124dab6002e30a88a
molki_ <161191998+molki-again@users.noreply.github.com>
2024-09-15 18:44:27 -0400

    initial commit

---
+++ fixer.js
@@ -0,0 +1,4 @@
+function refreshIframe() {
+    const iframe = document.getElementById('gameFrame');
+    iframe.src = iframe.src; 
+}
---
+++ static/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/static/nowgg/',
+    bare:'https://skibzy.global.ssl.fastly.net/bear/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
